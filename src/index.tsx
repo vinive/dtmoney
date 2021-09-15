5593ec87488fcebd130301de4dbe340cec4f2f5f
vinive <vinicius_ve_@hotmail.com>
2021-09-15 09:01:36 -0300

    dtmoney finalizado

---
+++ src/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
+import App from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          amount: 10000,
+          category: 'Desenvolvimento',
+          createdAt: new Date('2021-08-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          amount: 1200,
+          category: 'Casa',
+          createdAt: new Date('2021-08-13 014:00:00'),
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transactions')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    });
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
